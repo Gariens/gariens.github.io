b04fda1df979c9fc1e74fb7e4691c5a049380f92
Gariens <guy_ariens@hotmail.com>
2023-10-17 21:46:56 +0200

    new build

--- paper-kit-react-main/src/index.js
+++ paper-kit-react-main/src/index.js
@@ -26,11 +26,6 @@ import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 // pages
 import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import MosiacPage from "components/Mosiac/Mosiac.js"
 // others
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -39,11 +34,6 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/index" element={<Index />} />
-      <Route path="/MosiacPage" element={<MosiacPage />} />
-      <Route path="/nucleo-icons" element={<NucleoIcons />} />
-      <Route path="/landing-page" element={<LandingPage />} />
-      <Route path="/profile-page" element={<ProfilePage />} />
-      <Route path="/register-page" element={<RegisterPage />} />
       <Route path="*" element={<Navigate to="/index" replace />} />
     </Routes>
   </BrowserRouter>
